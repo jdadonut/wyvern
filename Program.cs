043ce4279dd1270f21c426a5b9ce7f75a68efb23
Jai L <47060357+jdadonut@users.noreply.github.com>
2021-08-28 23:49:27 -0400

    :flushed:

--- Program.cs
+++ Program.cs
@@ -9,19 +9,20 @@ namespace Wyvern
         static Config config = Json.Deserialize<Config>(File.ReadAllText("config.json"));
         static void Main(string[] args)
         {
-            Wyvern wyvern = new Wyvern().WithDatabase(new WyvernDatabase(
-                new WyvernDatabaseOptions(
-                    DatabaseType.MongoDB,
-                    CacheType.Memory,
-                    new MongoConnectionSettings(
-                        config.Mongo.Username,
-                        config.Mongo.Password,
-                        config.Mongo.ClusterUri,
-                        config.Mongo.Database,
-                        config.Mongo.retryWrites
-                    )
-                )
-            ));
+            Wyvern wyvern = new Wyvern();
+            // .WithDatabase(new WyvernDatabase(
+            //     new WyvernDatabaseOptions(
+            //         DatabaseType.MongoDB,
+            //         CacheType.Memory,
+            //         new MongoConnectionSettings(
+            //             config.Mongo.Username,
+            //             config.Mongo.Password,
+            //             config.Mongo.ClusterUri,
+            //             config.Mongo.Database,
+            //             config.Mongo.retryWrites
+            //         )
+            //     )
+            // ));
             wyvern.Start(config.Logging.LogLevel);
         }
 
--- src/Wyvern/Wyvern.cs
+++ src/Wyvern/Wyvern.cs
@@ -1,21 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Wyvern.Database;
 using Microsoft.AspNetCore;
+using System.Threading;
 namespace Wyvern
 {
   private WyvernDatabase db;
   public class Wyvern
   {
+    Thread asp;
     public Wyvern()
     {
-      
+      BootstrapAspNet();
     }
+    public void BootstrapAspNet()
+    {
+      this.asp = new Thread(() =>
+      {
+        WebHostBuilder app = new WebHostBuilder()
+        .UseKestrel()
+        .UseStartup<WyvernWebStartup>()
+        app.UseDeveloperExceptionPage();
+        Configure(app)
+        app.Build().Run();
+      });
+    }
+    public void Configure(IApplicationBuilder app)
+    {
+      app.UseStaticFiles();
+      app.UseRouting();
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapGet("/", async context =>
+        {
+          await context.Response.WriteAsync("Hello World!");
+        });
+      });
+    }
+
     public Wyvern WithDatabase(WyvernDatabase database)
     {
       this.db = database;
     }
     public void Start()
     {
-      
+      BuildDepencies();
+      this.asp.Start();
     }
+    public void BuildDepencies()
+    {
+      return;
+      if (this.db == null)
+      {
+        throw new Exception("Wyvern requires a database to be set");
+      }
+
+    }
+
+
+
   }
 }
